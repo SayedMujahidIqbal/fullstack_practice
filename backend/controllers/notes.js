1e0147324940877fe0d53a6fea411706e856498c
Mujahid Iqbal <sayedmujahidiqbal@gmail.com>
2024-10-22 10:34:32 +0300

    defining backend root folder structure

---
+++ backend/controllers/notes.js
@@ -0,0 +1,54 @@
+const notesRouter = require('express').Router()
+const Note = require('../models/note')
+
+
+notesRouter.get('/', (req, res) => {
+  Note.find({}).then(notes => {
+    res.json(notes)
+  })
+})
+
+notesRouter.get('/api/notes/:id', (req, res, next) => {
+  Note.findById(req.params.id).then(note => {
+    if(note){
+      res.json(note)
+    } else {
+      res.status(404).end()
+    }
+  })
+    .catch(error => next(error))
+})
+
+notesRouter.post('/api/notes', (req, res, next) => {
+
+  const body = req.body
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+  note.save().then(savedNote => {
+    res.json(savedNote)
+  })
+    .catch(error => next(error))
+})
+
+notesRouter.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+notesRouter.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note.findByIdAndUpdate(request.params.id, { content, important }, { new: true, runValidators: true, context: 'query' })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = notesRouter
