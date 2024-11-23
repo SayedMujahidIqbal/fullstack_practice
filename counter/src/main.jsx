c2365aa970f4d906b1e279c2c79c40376623d00d
Mujahid Iqbal <sayedmujahidiqbal@gmail.com>
2024-11-23 14:17:34 +0300

    backend with json server added

--- counter/src/main.jsx
+++ counter/src/main.jsx
@@ -1,18 +1,22 @@
 import { createRoot } from 'react-dom/client'
-import App from './App'
-import {  legacy_createStore as createStore, combineReducers } from "redux"
-import noteReducer from "./reducers/noteReducer"
 import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import App from './App'
+import noteService from './services/notes'
+import noteReducer, { setNotes } from "./reducers/noteReducer"
 import filterReducer from './reducers/filterReducer'
 
-const reducer = combineReducers({
-    notes: noteReducer,
-    filter: filterReducer
+
+const store = configureStore({
+    reducer: {
+        notes: noteReducer,
+        filter: filterReducer
+    }
 })
 
-const store = createStore(reducer)
-
-console.log(store.getState())
+noteService.getAll().then(notes => 
+    store.dispatch(setNotes(notes))
+)
 
 createRoot(document.getElementById('root')).render(
     <Provider store={store}>
