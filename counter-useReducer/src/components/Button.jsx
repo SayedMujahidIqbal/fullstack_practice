8c8632600f047a59ba5c62b621588e52f897346e
Mujahid Iqbal <sayedmujahidiqbal@gmail.com>
2024-11-25 09:57:40 +0300

    useReducer and useContext hooks used

---
+++ counter-useReducer/src/components/Button.jsx
@@ -0,0 +1,10 @@
+import { useCounterDispatch } from "../CounterContext"
+
+const Button = ({ type, label }) => {
+    const dispatch =  useCounterDispatch()
+    return(
+        <button onClick={() => dispatch({ type })}>{label}</button>
+    )
+}
+
+export default Button
