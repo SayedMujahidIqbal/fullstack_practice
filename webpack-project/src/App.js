c23a8021248cbcef529f77084860d2c4080343e9
Mujahid Iqbal <sayedmujahidiqbal@gmail.com>
2024-11-27 16:11:08 +0300

    webpack commit

--- webpack-project/src/App.js
+++ webpack-project/src/App.js
@@ -1,7 +1,20 @@
+import { useState } from "react"
+
 const App = () => {
+    const [counter, setCounter] = useState(0)
+    const [values, setValues] = useState([])
+
+    const handleClick = () => {
+        setCounter(counter + 1)
+        setValues(values.concat(counter))
+    }
+
     return (
-        <div>
-            <h2>hello webpack</h2>
+        <div className="container">
+            <h2>hello webpack {counter} clicks</h2>
+            <button onClick={handleClick}>
+                press
+            </button>
         </div>
     )
 }
